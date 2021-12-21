af35754baf439ae6ecebd82b7131c6ed12850940
Ralph Recto <recto.ralph@gmail.com>
2021-12-20 19:24:50 -0500

    Add GameStats struct

--- src/main.rs
+++ src/main.rs
@@ -14,32 +14,33 @@ lazy_static!{
 }
 
 #[derive(Debug)]
-struct GameStats<'a> {
-    player_id: &'a str,
-    player_name: &'a str,
-    game_id: &'a str,
-    stats: HashMap<&'a str, &'a str>
+struct PlayerGameStats {
+    player_id: String,
+    player_name: String,
+    stats: HashMap<String, String>
+}
+
+#[derive(Debug)]
+struct GameStats {
+    game_id: String,
+    player_stats: Vec<PlayerGameStats>
 }
 
 fn selector(selector_str: &str) -> Selector {
     Selector::parse(selector_str).unwrap()
 }
 
-fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) -> Vec<GameStats<'a>> {
-    let game_link_selector = selector("link[rel=canonical]");
-    let game_link = html_doc.select(&game_link_selector).next().unwrap().value().attr("href").unwrap();
-    let game_id = GAME_ID_REGEX.captures(game_link).unwrap().get(1).unwrap().as_str();
-
+fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) -> Vec<PlayerGameStats> {
     let table_selector = selector(table_selector_str);
     let table_elt= html_doc.select(&table_selector).next().unwrap();
 
     let tbody_selector= selector("tbody");
     let table_body = table_elt.select(&tbody_selector).next().unwrap();
 
-    let mut retlist: Vec<GameStats> = vec![];
+    let mut retlist: Vec<PlayerGameStats> = vec![];
     'rowlabel: for table_row in table_body.children() {
 
-        let mut player_data: HashMap<&str, &str> = HashMap::new();
+        let mut player_data: HashMap<String, String> = HashMap::new();
 
         for table_data_noderef in table_row.children() {
             let table_data_elt = match table_data_noderef.value() {
@@ -51,7 +52,7 @@ fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) ->
 
             match table_data_elt.attr("data-append-csv") {
                 Some(id) => {
-                    player_data.insert("player_id", id);
+                    player_data.insert("player_id".to_string(), id.to_string());
                 },
                 None => ()
             };
@@ -59,7 +60,7 @@ fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) ->
             // Handle stat.
 
             let stat_name = match table_data_elt.attr("data-stat") {
-                Some(s) => s,
+                Some(s) => s.to_owned(),
                 None => {
                     // This indicates the table body row is not a data row.
                     continue 'rowlabel
@@ -79,19 +80,18 @@ fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) ->
 
             match data_val_elt {
                 Some(Node::Text(t)) => {
-                    player_data.insert(stat_name, t.text.as_ref());
+                    player_data.insert(stat_name, t.text.to_string());
                 },
                 _ => ()
             };
         }
 
         match (player_data.get("player_id"), player_data.get("player")) {
-            (Some(&player_id), Some(&player_name)) => {
-                retlist.push(GameStats {
-                    player_id,
-                    player_name,
+            (Some(player_id), Some(player_name)) => {
+                retlist.push(PlayerGameStats {
+                    player_id: player_id.clone(),
+                    player_name: player_name.clone(),
                     stats: player_data,
-                    game_id
                 });
             },
             _ => ()
@@ -99,6 +99,30 @@ fn parse_player_stats_table<'a>(html_doc: &'a Html, table_selector_str: &str) ->
     }
 
     retlist
+}
+
+fn parse_game_log<'a>(game_log_html_str: String) -> GameStats {
+    // TODO a lot of the stats tables below are commented!
+    // find a better way to uncomment them
+    let s_prime = game_log_html_str.replace("\n<!--", "").replace("\n-->", "");
+    let html_doc = Html::parse_document(&s_prime);
+
+    let mut player_stats: Vec<PlayerGameStats> = vec![];
+
+    let game_link_selector = selector("link[rel=canonical]");
+    let game_link = html_doc.select(&game_link_selector).next().unwrap().value().attr("href").unwrap();
+    let game_id = GAME_ID_REGEX.captures(game_link).unwrap().get(1).unwrap().as_str().to_string();
+
+    player_stats.extend(parse_player_stats_table(&html_doc, "#player_offense"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#player_defense"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#returns"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#kicking"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#passing_advanced"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#rushing_advanced"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#receiving_advanced"));
+    player_stats.extend(parse_player_stats_table(&html_doc, "#defense_advanced"));
+
+    GameStats { game_id, player_stats }
 }
 
 #[tokio::main]
@@ -115,23 +139,10 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // `file` claims the bytes are actually ISO-8859-1.
     // TODO find a valid way to convert to text
     let bytes = to_bytes(body).await.unwrap();
-    let s: String = bytes.iter().map(|&c| c as char).collect();
+    let game_log_html_str: String = bytes.iter().map(|&c| c as char).collect();
 
-    // TODO a lot of the stats tables below are commented!
-    // find a better way to uncomment them
-    let s_prime = s.replace("\n<!--", "").replace("\n-->", "");
-    let html_doc = Html::parse_document(&s_prime);
-
-    let offense_stats = parse_player_stats_table(&html_doc, "#player_offense");
-    let defense_stats = parse_player_stats_table(&html_doc, "#player_defense");
-    let returns_stats = parse_player_stats_table(&html_doc, "#returns");
-    let kicking_stats = parse_player_stats_table(&html_doc, "#kicking");
-    let passing_adv_stats = parse_player_stats_table(&html_doc, "#passing_advanced");
-    let rushing_adv_stats = parse_player_stats_table(&html_doc, "#rushing_advanced");
-    let receiving_adv_stats = parse_player_stats_table(&html_doc, "#receiving_advanced");
-    let defense_adv_stats = parse_player_stats_table(&html_doc, "#defense_advanced");
-
-    println!("{:?}", offense_stats);
+    let game_stats = parse_game_log(game_log_html_str);
+    println!("{:?}", game_stats);
 
     Ok(())
 }
